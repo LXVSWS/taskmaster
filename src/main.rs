2f84de0923bb1727b990103f82f9c228fbc09879
LXV <lxvsws@gmail.com>
2024-05-27 20:52:23 +0200

    save

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self, Write};
+use std::process::Command;
 
 fn main() {
     println!("Taskmaster");
@@ -7,15 +8,12 @@ fn main() {
 		io::stdout().flush().expect("Flush error");
 		let mut input = String::new();
 		io::stdin().read_line(&mut input).expect("Readline error");
-		if input == "test\n" {
-			print!("Command : {}", input);
-		}
-		else if input == "exit\n" || input == "quit\n" {
+		let input = input.trim_end();
+		if input == "exit" || input == "quit" {
 			break;
 		}
-		else {
-			println!("Unknow command");
-		}
+		let command = Command::new(input).output().expect("Launch command failed");
+		println!("{}", String::from_utf8_lossy(&command.stdout));
 	}
 	println!("Bye");
 }
